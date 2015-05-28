d269e5da1a274ca74685e4f086e90cc3a33bb020
sdxt911 <sdxt911@qq.com>
2015-05-28 22:12:09 +0800

    test

---
+++ ggg/src/com/mycompany/model/Student.java
@@ -0,0 +1,18 @@
+package com.mycompany.model;
+
+public class Student {
+	private String sname;
+	private int age;
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
